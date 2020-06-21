9430f8b45c32aa284e02596b2ad16a2ba04b794d
Kim Maida <kim@kmaida.io>
2020-06-21 12:02:29 -0400

    Begin implementing typescript

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-require('dotenv').config();
-const { App } = require('@slack/bolt');
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var dotenv = require("dotenv");
+dotenv.config();
+var bolt_1 = require("@slack/bolt");
 // Airtable
-const at = require('./data/airtable');
-// MongoDB
-const mdb = require('./data/mongodb');
-const mongoose = require('mongoose');
-
+var airtable_1 = require("./data/airtable");
+var mongoose = require("mongoose");
 /*------------------
   CREATE BOLT APP
 ------------------*/
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+var app = new bolt_1.App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
 });
-const port = process.env.PORT || 3000;
-
+var port = process.env.PORT || 3000;
 /*------------------
       MONGODB
 ------------------*/
@@ -22,39 +57,41 @@ const port = process.env.PORT || 3000;
 mongoose.set('useUnifiedTopology', true);
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-const mon = mongoose.connection;
+var mon = mongoose.connection;
 // Capture connection errors
 mon.on('error', console.error.bind(console, 'MongoDB Connection Error. Please make sure that', process.env.MONGO_URI, 'is running.'));
 // Open connection
 mon.once('open', function () {
-  console.info('Connected to MongoDB:', process.env.MONGO_URI);
+    console.info('Connected to MongoDB:', process.env.MONGO_URI);
 });
-
 /*------------------
   SET UP MODAL IX
 ------------------*/
 require('./ix/modal')(app);
-require('./ix/modal-view-submit')(app, at);
-
+require('./ix/modal-view-submit')(app, airtable_1["default"]);
 /*------------------
   APP HOME OPENED
 ------------------*/
 require('./events/app-home-opened')(app);
-
 /*------------------
     APP MENTION
 ------------------*/
 require('./events/app-mention')(app);
-
 /*------------------
        BOT DM
 ------------------*/
-require('./events/message-im')(app, at);
-
+require('./events/message-im')(app, airtable_1["default"]);
 /*------------------
      START APP
 ------------------*/
-(async () => {
-  await app.start(port);
-  console.log(`⚡️ Slackbot is running on ${port}!`);
-})();
+(function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, app.start(port)];
+            case 1:
+                _a.sent();
+                console.log("\u26A1\uFE0F Slackbot is running on " + port + "!");
+                return [2 /*return*/];
+        }
+    });
+}); })();
---
+++ index.ts
@@ -0,0 +1,61 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { App } from '@slack/bolt';
+// Airtable
+import at from './data/airtable';
+// MongoDB
+import mdb from './data/mongodb';
+import * as mongoose from 'mongoose';
+
+/*------------------
+  CREATE BOLT APP
+------------------*/
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+const port = process.env.PORT || 3000;
+
+/*------------------
+      MONGODB
+------------------*/
+// Address server discovery deprecation warning
+mongoose.set('useUnifiedTopology', true);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+const mon = mongoose.connection;
+// Capture connection errors
+mon.on('error', console.error.bind(console, 'MongoDB Connection Error. Please make sure that', process.env.MONGO_URI, 'is running.'));
+// Open connection
+mon.once('open', function () {
+  console.info('Connected to MongoDB:', process.env.MONGO_URI);
+});
+
+/*------------------
+  SET UP MODAL IX
+------------------*/
+require('./ix/modal')(app);
+require('./ix/modal-view-submit')(app, at);
+
+/*------------------
+  APP HOME OPENED
+------------------*/
+require('./events/app-home-opened')(app);
+
+/*------------------
+    APP MENTION
+------------------*/
+require('./events/app-mention')(app);
+
+/*------------------
+       BOT DM
+------------------*/
+require('./events/message-im')(app, at);
+
+/*------------------
+     START APP
+------------------*/
+(async () => {
+  await app.start(port);
+  console.log(`⚡️ Slackbot is running on ${port}!`);
+})();
