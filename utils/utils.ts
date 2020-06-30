71aeababf72802018144ddeec1bb01ce387cdf6b
Kim Maida <yi.mihi@gmail.com>
2020-06-30 09:44:54 -0400

    More robust handling of ignore mention middleware to account for inconsistencies in Slack API (verified with Slack support)

--- utils/utils.ts
+++ utils/utils.ts
@@ -40,17 +40,23 @@ const clearNewline = (input: string): string => {
 
 /**
  * Message middleware: ignore some kinds of messages
+ * Ignore channel topic and edited message events
+ * (e.g., link unfurling is an edit / message_changed subtype)
+ * This has catches for inconsistencies in Slack API
  * @param {IObjectAny} event event object
  * @return {Promise<void>} continue if not ignored message type
  */
-const ignoreMention = async ({ event, next }: IObjectAny): Promise<void> => {
+const ignoreMention = async ({ message, event, next }: IObjectAny): Promise<void> => {
   const disallowedSubtypes = ['channel_topic', 'message_changed'];
-  const ignoreSubtype = disallowedSubtypes.indexOf(event.subtype) > -1;
-  const messageChanged = !!event.edited;
-  // If not ignored subtype and not an edited message event, proceed
-  if (!ignoreSubtype && !messageChanged) {
-    await next();
+  const ignoreSubtypeEvent = disallowedSubtypes.indexOf(event.subtype) > -1;
+  const ignoreSubtypeMessage = message && message.subtype && disallowedSubtypes.indexOf(message.subtype) > -1;
+  const ignoreEdited = !!event.edited;
+  // If mention should be ignored, return
+  if (ignoreSubtypeEvent || ignoreSubtypeMessage || ignoreEdited) {
+    return;
   }
+  // If mention should be processed, continue
+  await next();
 }
 
 export { validUrl, objNotEmpty, clearNewline, ignoreMention };
